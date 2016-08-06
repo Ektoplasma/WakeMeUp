374d788f318b4e51a50b9ba2af8d2817a6503e10
Ektoplasma <acrasidae@gmail.com>
2016-08-06 19:27:10 +0200

    modifscript

--- wakemeup_scripts/Members.class.php
+++ wakemeup_scripts/Members.class.php
@@ -4,7 +4,7 @@ require_once("easyCRUD.class.php");
 class Follower Extends Crud {
 
   # The table you want to perform the database actions on
-  protected $table = 'friends';
+  protected $table = 'members';
 
   # Primary Key of the table
   protected $pk  = 'id';
