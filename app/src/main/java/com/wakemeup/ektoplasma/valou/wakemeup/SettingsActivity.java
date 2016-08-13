fd8c8aa911a24ac09915b926676cf13bc09f0156
Valoulou <valentin.chaigneau@gmail.com>
2016-08-13 15:56:38 +0200

    Settings_page

--- app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/SettingsActivity.java
+++ app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/SettingsActivity.java
@@ -1,18 +1,53 @@
 package com.wakemeup.ektoplasma.valou.wakemeup;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 /**
  * Created by Valentin on 11/08/2016.
  */
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.settings_screen);
+        SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
+        EditTextPreference editTextPref = (EditTextPreference) findPreference("prefReveilDefault");
+        editTextPref
+                .setSummary(sp.getString("prefReveilDefault", "Lien YouTube"));
 
     }
+
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        Preference pref = findPreference(key);
+        if (pref instanceof EditTextPreference) {
+            EditTextPreference etp = (EditTextPreference) pref;
+            pref.setSummary(etp.getText());
+        }
+        else if (pref instanceof ListPreference) {
+            ListPreference listp = (ListPreference) pref;
+            pref.setSummary(listp.getEntry());
+        }
+    }
+
 }
