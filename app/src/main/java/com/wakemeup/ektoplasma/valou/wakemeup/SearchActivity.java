d829bd79ca4f32f0fb65aa02a44eb1a24eeae712
Valoulou <valentin.chaigneau@gmail.com>
2016-08-29 16:32:22 +0200

    Recherche

--- app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/SearchActivity.java
+++ app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/SearchActivity.java
@@ -13,19 +13,6 @@ public class SearchActivity extends Activity implements SearchView.OnQueryTextLi
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        handleIntent(getIntent());
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-        }
     }
 
     @Override
