49582247789c6707ae398e1b066c140554157167
Valoulou <valentin.chaigneau@gmail.com>
2016-08-30 22:23:49 +0200

    Listes

--- app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/DemandeAmiActivity.java
+++ app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/DemandeAmiActivity.java
@@ -1,6 +1,7 @@
 package com.wakemeup.ektoplasma.valou.wakemeup;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +9,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Valentin on 30/08/2016.
  */
-public class DemandeAmiActivity extends AppCompatActivity {
+public class DemandeAmiActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demande_ami);
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Valou");
+        list.add("Théo");
+
+        CustomAdapterDemandeAmi adapter = new CustomAdapterDemandeAmi(list, this);
+
+        ListView lView = (ListView)findViewById(R.id.listDemandeAmi);
+        lView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/MessageActivity.java
+++ app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/MessageActivity.java
@@ -2,15 +2,29 @@ package com.wakemeup.ektoplasma.valou.wakemeup;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Valentin on 30/08/2016.
  */
 public class MessageActivity  extends AppCompatActivity {
 
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_message);
+        setContentView(R.layout.message);
+        mainListView = (ListView) findViewById( R.id.listMessage );
+        ArrayList<String> messagelist = new ArrayList<String>();
+        listAdapter = new ArrayAdapter<String>(this, R.layout.message_row, messagelist);
+        listAdapter.add( "Paul : Pas mal ton reveil" );
+        listAdapter.add( "Pluto : Mec... Really ?" );
+
+        mainListView.setAdapter( listAdapter );
     }
 }
