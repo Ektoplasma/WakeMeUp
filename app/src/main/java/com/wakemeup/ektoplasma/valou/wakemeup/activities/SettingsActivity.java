0ef6b7f0d70af91f5cd5936cc3bd41b345b056d8
Ektoplasma <acrasidae@gmail.com>
2016-10-20 17:36:07 +0200

    pref dynamique (sale)

--- app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/activities/SettingsActivity.java
+++ app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/activities/SettingsActivity.java
@@ -2,12 +2,19 @@ package com.wakemeup.ektoplasma.valou.wakemeup.activities;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.wakemeup.ektoplasma.valou.wakemeup.R;
 import com.wakemeup.ektoplasma.valou.wakemeup.utilities.Caller;
@@ -18,17 +25,142 @@ import com.wakemeup.ektoplasma.valou.wakemeup.utilities.Caller;
 public class SettingsActivity extends PreferenceActivity implements
         SharedPreferences.OnSharedPreferenceChangeListener {
 
+    private LinearLayout rootView;
+
+    private LinearLayout titleView;
+
+    private ListView preferenceView;
+
+    private TextView textView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.settings_screen);
+        /*addPreferencesFromResource(R.xml.settings_screen);
         SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
         EditTextPreference editTextPref = (EditTextPreference) findPreference("prefReveilDefault");
         editTextPref
                 .setSummary(sp.getString("prefReveilDefault", "Lien YouTube"));
         ListPreference lp = (ListPreference) findPreference("prefWhoWakeMe");
-        lp.setSummary(sp.getString("prefWhoWakeMe", "Tout le monde"));
+        lp.setSummary(sp.getString("prefWhoWakeMe", "Tout le monde"));*/
+
+        rootView = new LinearLayout(this);
+
+        rootView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+
+        rootView.setOrientation(LinearLayout.VERTICAL);
+
+        textView = new TextView(this);
+
+        textView.setText(R.string.toto);
+
+        preferenceView = new ListView(this);
+
+        preferenceView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+
+        preferenceView.setId(android.R.id.list);
+
+
+
+        PreferenceScreen screen = createPreferenceHierarchy();
+
+        screen.bind(preferenceView);
+
+        preferenceView.setAdapter(screen.getRootAdapter());
+
+        rootView.addView(textView);
+
+        rootView.addView(preferenceView);
+
+        this.setContentView(rootView);
+
+        setPreferenceScreen(screen);
+
+    }
+
+    private PreferenceScreen createPreferenceHierarchy() {
+
+        // Root
+
+        PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+
+
+        // Inline preferences
+
+        PreferenceCategory cat1 = new PreferenceCategory(this);
+
+        cat1.setTitle("Cat 1");
+
+        root.addPreference(cat1);
+
+
+
+        for(int i = 0; i < 5; i ++)
+
+        {
+
+            // Toggle preference
+
+            CheckBoxPreference togglePref = new CheckBoxPreference(this);
+
+            togglePref.setKey("toggle_check" + Integer.toString(i));
+
+            togglePref.setTitle("Toggle Me");
+
+            togglePref.setChecked(prefs.getBoolean("toggle_check" + Integer.toString(i), false));
+
+            togglePref.setSummary("Checkbox " + Integer.toString(i));
+
+            cat1.addPreference(togglePref);
+
+        }
+
+
+
+        // Inline preferences
+
+        PreferenceCategory cat2 = new PreferenceCategory(this);
+
+        cat2.setTitle("Cat 2");
+
+        root.addPreference(cat2);
+
+
+
+        for(int i = 6; i < 13; i ++)
+
+        {
+
+            // Toggle preference
+
+            CheckBoxPreference togglePref = new CheckBoxPreference(this);
+
+            togglePref.setKey("toggle_check" + Integer.toString(i));
+
+            togglePref.setTitle("Toggle Me");
+
+            togglePref.setChecked(prefs.getBoolean("toggle_check" + Integer.toString(i), false));
+
+            togglePref.setSummary("Checkbox " + Integer.toString(i));
+
+            cat2.addPreference(togglePref);
+
+        }
+
+        PreferenceCategory cat3 = new PreferenceCategory(this);
+        root.addPreference(cat3);
+        EditTextPreference truc = new EditTextPreference(this);
+        truc.setKey("edittext");
+        truc.setTitle("Edit Me");
+
+        cat3.addPreference(truc);
+
+
+        return root;
 
     }
 
