ded7aef552ec8183f03b3bd87628ad83363d163c
valoulou <valentin.chaigneau@gmail.com>
2016-11-26 16:37:53 +0100

    Message

--- app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/activities/MessageActivity.java
+++ app/src/main/java/com/wakemeup/ektoplasma/valou/wakemeup/activities/MessageActivity.java
@@ -52,6 +52,7 @@ public class MessageActivity  extends AppCompatActivity {
             listpseudo.add(pseudo);
             listmessage.add(message);
         }
+        //temporaire
         listpseudo.add("Valou");
         listmessage.add("C'est celui qui le dit...");
         CustomAdapterMessage adapter = new CustomAdapterMessage(listpseudo, listmessage, this);
